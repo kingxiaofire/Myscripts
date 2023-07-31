a25fc13b67cdd492a9841ccd38ebe4651753391e
Yan Wang <1179271336@qq.com>
2023-07-31 21:44:31 +0800

    Create 2750鸡兔同笼.cpp

---
+++ ACMcode/2750鸡兔同笼.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%2!=0) printf("0 0\n");
+	else if(a%4!=0) printf("%d %d\n",a/4+1,a/2);
+	else printf("%d %d\n",a/4,a/2);
+	return 0;
+}
